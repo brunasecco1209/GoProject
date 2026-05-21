dd6cee2c404c515ce76db2ab0e94dd795c74168b
Breno-Herculano <bherculano691@gmail.com>
2026-05-21 20:22:18 -0300

    Add Função validação CPF

--- main.go
+++ main.go
@@ -30,3 +30,73 @@ type Update struct {
 
 	} `json:"message"`
 }
+
+// ---------------- FUNÇÃO CPF ---------------- //
+
+func validarCPF(cpf string) bool {
+
+	cpf = strings.ReplaceAll(cpf, ".", "")
+	cpf = strings.ReplaceAll(cpf, "-", "")
+
+	if len(cpf) != 11 {
+		return false
+	}
+
+	for _, c := range cpf {
+
+		if c < '0' || c > '9' {
+			return false
+		}
+	}
+
+	todosIguais := true
+
+	for i := 1; i < 11; i++ {
+
+		if cpf[i] != cpf[0] {
+
+			todosIguais = false
+			break
+		}
+	}
+
+	if todosIguais {
+		return false
+	}
+
+	// Primeiro dígito
+	soma := 0
+
+	for i := 0; i < 9; i++ {
+
+		soma += int(cpf[i]-'0') * (10 - i)
+	}
+
+	digito1 := (soma * 10) % 11
+
+	if digito1 == 10 {
+		digito1 = 0
+	}
+
+	// Segundo dígito
+	soma = 0
+
+	for i := 0; i < 10; i++ {
+
+		soma += int(cpf[i]-'0') * (11 - i)
+	}
+
+	digito2 := (soma * 10) % 11
+
+	if digito2 == 10 {
+		digito2 = 0
+	}
+
+	if digito1 == int(cpf[9]-'0') &&
+		digito2 == int(cpf[10]-'0') {
+
+		return true
+	}
+
+	return false
+}
